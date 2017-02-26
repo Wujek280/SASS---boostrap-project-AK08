a0a6a9eaff7fe141b6a21babcf6ee532bb591127
Wujek <mjwojcik94@gmail.com>
2017-02-26 10:40:09 +0100

    13k szitu

--- gulpfile.js
+++ gulpfile.js
@@ -1,67 +1,54 @@
-'use strict';
-<<<<<<< HEAD
-
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var cssnano = require('gulp-cssnano');
+var uglify = require('gulp-uglify');
 
-gulp.task('siema',function() { //nazwa taska 
-   
-   console.log('Siema swiecie!');
-   
+
+// autorefresh
+gulp.task('browserSync', function() {
+   'use strict';
+  browserSync.init({
+    server: {
+      baseDir: 'app'
+    },
+  })
 });
 
-
-//KOMPILACJA SCSS >> CSS
-gulp.task('sass',function(){ //kompilator SASS
-   
-   return gulp.src('app/scss/**/*.scss') //przeszukuje wszystkie w scss
-   .pipe(sass())
-   .on('error', function (err) {
-            console.log(err.toString());
-
-            this.emit('end');
-        })
-   .pipe(gulp.dest('app/css'));
-   
-});
-
-//OBSERWOWANIE ZMIAN W PLIKACH
-gulp.task('watch',function(){ //
-   
-// obserwuj  >>CO i gdzie       >> TABLICA TASKÓW
- gulp.watch('app/scss/**/*.scss', ['sass']);
-   
-=======
-var gulp = require('gulp'),
- sass = require('gulp-sass'),
- browserSync = require('browser-sync').create();
-
-gulp.task('siema', function () { //nazwa taska 
-   console.log('Siema swiecie!');
-});
-
-//KOMPILACJA SCSS >> CSS
-gulp.task('sass', function () { //kompilator SASS
-   return gulp.src('app/scss/**/*.scss') 
-      .pipe(sass())
-      .on('error', function (err) {
-         console.log(err.toString());
-         this.emit('end');
-      })
-      .pipe(gulp.dest('app/css'))
-      .pipe(browserSync.reload({
+/* Kompilacja plików scss do css */
+gulp.task('sass', function(){
+	return gulp.src('app/scss/main.scss')
+	.pipe(sass())
+	.pipe(gulp.dest('app/css'))
+	.pipe(browserSync.reload({
       stream: true
-   }));
+    }));
 });
 
-//OBSERWOWANIE ZMIAN W PLIKACH
-gulp.task('watch', function () { //
-   browserSync.init({
-      server: {
-         baseDir:'app'
-      },
-   })
-      // obserwuj  >>CO i gdzie       >> TABLICA TASKÓW
-      gulp.watch('app/scss/**/*.scss', ['sass']);
->>>>>>> parent of 68e2543... shiet
+//kopiowanie do /dist CSS i mumifikacjia
+gulp.task('cssnano', function(){
+   return gulp.src('app/**/*.css')
+   .pipe(cssnano())
+   .pipe(gulp.dest('dist')); 
+});
+
+//kopiowanie do /dist html'ów
+gulp.task('dist', function(){
+   return gulp.src('app/**/*.html')
+   .pipe(gulp.dest('dist'));   
+});
+
+//
+gulp.task('uglifyjs', function(){
+   return gulp.src('app/js/*.js')
+   .pipe(uglify())
+   .pipe(gulp.dest('dist/js'));   
+});
+
+//////////////////////////////////
+/* Obserwowanie zmian w plikach */
+//////////////////////////////////
+gulp.task('build', ['browserSync', 'sass', 'cssnano'], function(){
+	gulp.watch('app/scss/**/*.scss',['sass']);
+	gulp.watch('app/*.html', browserSync.reload); 
 });
